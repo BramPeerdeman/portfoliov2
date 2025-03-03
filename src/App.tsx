556b1d7562aa15df98633cbfc3c88aa94c746f61
Bram <bram@fluet.dev>
2025-03-03 18:46:43 +0100

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,9 @@ import About from './components/about/About';
 import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
-import Testimonials from './components/testimonials/Testimonial';
+// import Testimonials from './components/testimonials/Testimonial';
 import Contact from './components/contact/Contact';
-import Blog from './components/blog/Blog';
+// import Blog from './components/blog/Blog';
 
 const App = () => 
   {
